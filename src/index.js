459b1293ca9a94e747061b4eee54ed6051c72567
Dani Sadorf <dani.noelani@gmail.com>
2019-06-13 11:29:14 -0600

    Changes to deploy to Heroku

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // module.exports = async () => {
 //   return 'Hello, world'
 // }
+const micro = require('micro')
 const { send, json } = require('micro')
 const { router, get, post, put, del } = require('microrouter')
 const cors = require('micro-cors')()
- 
+
 const hello = (req, res) => send(res, 200, `Hello ${req.params.who}`)
- 
+
 const notfound = (req, res) => send(res, 404, 'Not found route')
 
 const Datastore = require('nedb-promise')
@@ -15,9 +16,9 @@ const db = new Datastore({
   autoload: true
 });
 
- 
-module.exports = cors(router(
-  get('/hello/:who', hello), 
+
+const server = micro(cors(router(
+  get('/hello/:who', hello),
   post('/', async (req, res) => {
     const js = await json(req)
     const inserted = await db.insert(js)
@@ -29,16 +30,19 @@ module.exports = cors(router(
   put('/:id', async (req, res) => {
     console.log(req.params.id)
     const js = await json(req)
-    await db.update({_id: req.params.id}, {...js})
+    await db.update({ _id: req.params.id }, { ...js })
     console.log(js)
-    return await db.findOne({_id: req.params.id})
+    return await db.findOne({ _id: req.params.id })
   }),
   del('/:id', async (req, res) => {
-    await db.remove({_id: req.params.id})
-    return {ok: true}
+    await db.remove({ _id: req.params.id })
+    return { ok: true }
   }),
   get('/:id', async (req, res) => {
-    return await db.findOne({_id: req.params.id})
+    return await db.findOne({ _id: req.params.id })
   }),
   get('/*', notfound)
-  ))
+)))
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT)
